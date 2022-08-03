fb42b8bee88dd7d74ef9af17f482b853884a91ae
marco land <marco.anmeldung@gmail.com>
2022-08-03 15:30:47 +0200

    change: matomo url

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ Vue.config.productionTip = false;
 Vue.config.devtools = true;
 
 Vue.use(VueMatomo, {
-  host: "https://stats.ungroup.group",
+  host: "https://stats.marco.land",
   siteId: 10
 });
 
