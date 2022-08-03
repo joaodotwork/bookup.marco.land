a95a231d796a5ce359a352d0cd750bab45fc2c73
marco land <marco.anmeldung@gmail.com>
2022-08-03 15:18:25 +0200

    add: matomo

--- src/main.js
+++ src/main.js
@@ -1,15 +1,20 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import Vue from "vue";
+import App from "./App";
+import VueMatomo from "vue-matomo";
 
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 Vue.config.devtools = true;
+
+Vue.use(VueMatomo, {
+  host: "https://stats.ungroup.group/",
+  siteId: 10
+});
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   components: { App },
-  template: '<App/>'
-})
+  template: "<App/>"
+});
